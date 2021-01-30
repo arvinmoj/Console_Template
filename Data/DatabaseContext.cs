c83f4dd8adae4689e8b33506fde3d2228bbb1e82
arvinmoj <52661749+arvinmoj@users.noreply.github.com>
2021-01-30 12:12:39 +0330

    first commit

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using Microsoft.EntityFrameworkCore;
+namespace Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base()
+        {
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: "Password=A!1q2w3e!;Persist Security Info=True;User ID=SA; Initial Catalog = Console_Application; Data Source=ASUS\\SQLSERVER");
+        }
+
+        public DbSet<Model> Models { get; set; }
+
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Model
+    {
+        public Model() : base()
+        {
+              Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Family { get; set; }
+
+        public int Age { get; set; }
+    }
+}
